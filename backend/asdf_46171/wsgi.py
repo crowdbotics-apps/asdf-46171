30ccb34f4a583976a473657c807191c62f51a18f
Crowdbotics <team@crowdbotics.com>
2024-02-08 23:38:55 +0000

    Initial commit

---
+++ backend/asdf_46171/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for asdf_46171 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "asdf_46171.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "asdf-46171",
+  url: "https://asdf-46171.botics.co",
+  api: "https://asdf-46171.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
